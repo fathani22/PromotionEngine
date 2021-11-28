bdb55ac0846368a8d901d1daf6e39c2b9aa0f892
fathani22 <fareenathani22@gmail.com>
2021-11-29 00:51:50 +0530

    Changes related to application class

---
+++ src/main/java/com/promotion/PromotionEngineApplication.java
@@ -0,0 +1,10 @@
+package main.java.com.promotion;
+
+public class PromotionEngineApplication {
+
+	public static void main(String[] args) {
+		System.out.print("Welcome to Promotion Engine Application !");
+
+	}
+
+}
